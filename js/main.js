7699ac6fccfc24dd3b8daf85c6445d68f1444147
linh <linhlinhkook00@gmail.com>
2023-05-12 17:43:12 +0700

    updaete

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const swiperNewProduct = new Swiper(".new-product", {
     //     disableOnInteraction: false,
     // },
     loop:true,
-    loopedSlides: 50,
+    // loopedSlides: 50,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -17,21 +17,122 @@ const swiperNewProduct = new Swiper(".new-product", {
         // when window width is >= 480px
         120: {
             slidesPerView: 2,
-            spaceBetween: 2
+            spaceBetween: 0
         },
         // when window width is >= 640px
        // when window width is >= 640px
         740: {
             slidesPerView: 2,
-            spaceBetween: 10
+            spaceBetween: 0
         },
         1023: {
             slidesPerView: 7,
+            spaceBetween: 0
+        }
+    },
+});
+
+const swiperDealProduct = new Swiper(".deals-product", {
+    slidesPerView: 7,
+    spaceBetween: 0,
+    // auto
+    // autoplay: {
+    //     delay: 500,
+    //     disableOnInteraction: false,
+    // },
+    loop:true,
+    // loopedSlides: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 2000,
+    breakpoints: {
+        // when window width is >= 480px
+        120: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        // when window width is >= 640px
+       // when window width is >= 640px
+        740: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 0
+        }
+    },
+});
+
+const swiperBanner3 = new Swiper(".banner-3-product", {
+    slidesPerView: 7,
+    spaceBetween: 0,
+    // auto
+    // autoplay: {
+    //     delay: 500,
+    //     disableOnInteraction: false,
+    // },
+    loop:true,
+    // loopedSlides: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 2000,
+    breakpoints: {
+        // when window width is >= 480px
+        120: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        // when window width is >= 640px
+       // when window width is >= 640px
+        740: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        1023: {
+            slidesPerView: 3,
             spaceBetween: 20
         }
     },
 });
 
+const swiperBanner2 = new Swiper(".banner-2-product", {
+    slidesPerView: 7,
+    spaceBetween: 0,
+    // auto
+    // autoplay: {
+    //     delay: 500,
+    //     disableOnInteraction: false,
+    // },
+    loop:true,
+    // loopedSlides: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 2000,
+    breakpoints: {
+        // when window width is >= 480px
+        120: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        // when window width is >= 640px
+       // when window width is >= 640px
+        740: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        1023: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        }
+    },
+});
 
 const swiperBlogs = new Swiper(".swiper-blogs", {
     slidesPerView: 7,
@@ -97,6 +198,40 @@ const swiperCategories = new Swiper(".categories-product", {
         1023: {
             slidesPerView: 9,
             spaceBetween: 0,
+        }
+    },
+});
+
+const swiperSectionProduct = new Swiper(".section-product", {
+    slidesPerView: 7,
+    spaceBetween: 0,
+    // auto
+    // autoplay: {
+    //     delay: 500,
+    //     disableOnInteraction: false,
+    // },
+    loop:true,
+    // loopedSlides: 50,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 2000,
+    breakpoints: {
+        // when window width is >= 480px
+        120: {
+            slidesPerView: 2,
+            spaceBetween: 0
+        },
+        // when window width is >= 640px
+       // when window width is >= 640px
+        740: {
+            slidesPerView: 2,
+            spaceBetween: 0
+        },
+        1023: {
+            slidesPerView: 6,
+            spaceBetween: 0
         }
     },
 });
